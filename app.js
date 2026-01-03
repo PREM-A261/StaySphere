4c08bc203e1c4f8344c9e1605888021e843f181b
PREM-A261 <pawarprem5555@gmail.com>
2026-01-03 22:50:07 +0530

    listing routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
+const path = require('path');
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/staysphere';
 main()
@@ -16,23 +17,52 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('HI, I am Root!');
 });
 
-app.get('/testListings', async (req, res) => {
-    let sampleListings = new Listing({
-        title: 'My New Villa',
-        description: 'By the Beach',
-        price: 1200,
-        location: 'Calangute, Goa',
-        country: 'India',
-    });
-
-    await sampleListings.save();
-    console.log('Sample Listing Saved');
-    res.send('Sussessfully added listing');
+//Index Route 
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
 });
+
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+// app.get('/testListings', async (req, res) => {
+//     let sampleListings = new Listing({
+//         title: 'My New Villa',
+//         description: 'By the Beach',
+//         price: 1200,
+//         location: 'Calangute, Goa',
+//         country: 'India',
+//     });
+
+//     await sampleListings.save();
+//     console.log('Sample Listing Saved');
+//     res.send('Sussessfully added listing');
+// });
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
