98800921d87040e4be28d6314ff2cb06c74a084c
PREM-A261 <pawarprem5555@gmail.com>
2026-01-12 00:09:19 +0530

    Phase 1 completed

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js");
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
+const { listingSchema } = require('./schema.js');
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/staysphere';
 main()
@@ -30,11 +33,21 @@ app.get('/', (req, res) => {
     res.send('HI, I am Root!');
 });
 
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(',');
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
 //Index Route 
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-});
+}));
 
 //New Route
 app.get("/listings/new", (req, res) => {
@@ -42,40 +55,45 @@ app.get("/listings/new", (req, res) => {
 });
 
 //show route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync( async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
-});
+}));
 
 //Create Route
-app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-});
+app.post("/listings", 
+    validateListing,
+    wrapAsync(async (req, res, next) => {
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    })
+);
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id",
+    validateListing,
+    wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect("/listings");
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
-});
+}));
 
 // app.get('/testListings', async (req, res) => {
 //     let sampleListings = new Listing({
@@ -91,6 +109,22 @@ app.delete("/listings/:id", async (req, res) => {
 //     res.send('Sussessfully added listing');
 // });
 
+app.all('/', (req, res, next) => {
+    throw new ExpressError("listing.image must be a string", 400);
+});
+
+app.use((err, req, res, next) => {
+    let statusCode = err.statusCode;
+
+    if (typeof statusCode !== "number") {
+        statusCode = 500;
+    }
+
+    const message = err.message || "Something went wrong!";
+
+    res.status(statusCode).render("error.ejs", { message });
+        //res.status(statusCode).send(message);
+ });
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
 });
--- models/listing.js
+++ models/listing.js
@@ -11,7 +11,7 @@ const listingSchema = new Schema({
     filename: String,
     url: {
       type: String,
-      default: "https://images.unsplash.com/default",
+      default: "https://plus.unsplash.com/premium_photo-1744671718789-d165cf55baa0?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   },
     price: Number,
